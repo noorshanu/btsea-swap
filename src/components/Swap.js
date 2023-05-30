995b8cccdcb596df3708c22ff6d4e301a5f9ef17
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-05-31 02:02:52 +0530

    swap box

--- src/components/Swap.js
+++ src/components/Swap.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from "react";
 
 function Swap() {
   return (
-    <section className='flex justify-center items-center'>
-        <div>
-<h1 className='text-white'>swapbox</h1>
+    <section className="flex justify-center items-center">
+      <div>
+        <div className="text-white swap-box w-[400px] p-4">
+          <div className="flex justify-between">
+            <div className="flex gap-5">
+              <h2>SWAP</h2>
+              <h2>BUY</h2>
+            </div>
+            <h2>Setting</h2>
+          </div>
         </div>
-        </section>
-  )
+      </div>
+    </section>
+  );
 }
 
-export default Swap
+export default Swap;
